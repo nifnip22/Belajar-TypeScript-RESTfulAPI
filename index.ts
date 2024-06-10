3d9375cb8fe0a9a86b8c35245d18d3d84dc7ea9a
nifnip22 <proxigaming742@gmail.com>
2024-06-10 19:56:24 +0800

    Setup MySQL Database & Setup Prisma Schema Models

--- index.ts
+++ index.ts
@@ -34,4 +34,9 @@ console.log("Hello via Bun!");
     Setelah semuanya telah di install, selanjutnya kita akan membuat dokumentasi API Spec. Buat sebuah folder yang diberi nama 'doc'. Di dalam folder doc,
     buat file dalam bentuk markdown sesuai dengan fitur yang akan dibuat
 
+    Setelah membuat dokumentasi API Spec, selanjutnya kita akan setup database MySQL. Setelah membuat database, kita akan melakukan setup Prisma. Jalankan
+    perintah di terminal 'bunx prisma init --datasource-provider mysql'. Setelahnya ganti URL Database di .env dan buat model satu per satu pada schema prismanya.
+    Setelah membuat model maka kita akan melakukan migration dengan mengetikan perintah di terminal 'bunx prisma migrate dev' (pastikan dijalankan setiap membuat satu
+    model baru)
+
 */ 
